655a1f1227fb384c3e373a1eb1db5d8981e753a4
web-Hobbit <63592018+web-Hobbit@users.noreply.github.com>
2020-05-29 17:58:17 +0600

    Add files via upload

---
+++ contacts/contacts.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	 <link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link type="text/css" href="/css/contacts.css" rel="stylesheet">
+	<link type="text/css" href="/header/header.css" rel="stylesheet">
+	<link type="text/css" href="/footer/footer.css" rel="stylesheet">
+  </head>
+ <body>
+<div class="header">
+<?php include "C:/OSPanel/domains/Bellyful/header/header.php"; ?>
+</div>
+
+
+<main role="main" class="main">
+
+<div class="container">
+   <div class="Contacts_Title">  
+  <section class="text-center">
+           <div class="container">
+               <h4><b>Контакты</b></h4>
+            </div>
+        </section>
+</div>
+</div>
+
+
+ <div class="container">
+    <div class="adres">
+      <div class="row">
+        <div class="col-xs-12 col-sm-7 col-md-6 col-lg-4 col-xl-6">
+            <div class="d-flex flex-column" >
+                <p>
+                 <b>Наш адрес:</b> Адрес
+               </p>
+          <ul class="list-inline">
+           <li class="list-inline-item">
+              <a class="btn-floating btn-fb">
+                 <i class="fa fa-vk fa-2x" style="color:#F6E0AA;"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+               <a class="btn-floating btn-tw">
+                  <i class="fa fa-instagram fa-2x" style="color:#F6E0AA;"></i>
+               </a>
+            </li>
+           </ul>
+          
+           </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-7 col-md-6 col-lg-4 col-xl-6">
+            <div class="d-flex flex-column">
+                <p>
+            Равным образом рамки и место обучения кадров<br>
+            способствует подготовке и реализации дальнейших<br>
+            направлений развития. Равным образом постоянное<br>
+            информационное обеспечение нашей деятельности в<br>
+            в значительной степени обуславливает создание<br>
+            модели развития.<br>
+           </div>
+        </div>
+
+    </div>
+  </div>
+</div>
+</div>
+</main>
+
+<div class="map-responsive">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29932.274939145067!2d73.44056373747446!3d54.97451639516917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43aafde2f601090b%3A0x5eefc33861a69b1a!2z0J7QvNGB0LosINCe0LzRgdC60LDRjyDQvtCx0LsuLCDQoNC-0YHRgdC40Y8!5e0!3m2!1sru!2skz!4v1590742427501!5m2!1sru!2skz" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</div>
+
+
+
+
+</main>
+
+<div class="Footer">
+<?php include 'C:/OSPanel/domains/Bellyful/footer/footer.php'; ?>
+</div>
+
+ <!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="/js/popper.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/examples/vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="/js/main.js"></script>
+
+<script>
+	$(document).ready(function()
+	    {
+			$('#sidebarCollapse').on('click',function()
+			{
+				$('#sidebar').toggleClass('active');
+			});
+		});  
+
+</script>
+    
+<script>
+function openNav()
+{
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() 
+{
+    document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+    
+
+</body>
+</html>
