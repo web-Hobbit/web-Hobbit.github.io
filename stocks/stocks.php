a1719f2b85155df74c29456e435acf2e720cdb46
web-Hobbit <63592018+web-Hobbit@users.noreply.github.com>
2020-05-29 17:51:47 +0600

    Add files via upload

---
+++ stocks/stocks.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	 <link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link type="text/css" href="/css/stocks.css" rel="stylesheet">
+	<link type="text/css" href="/header/header.css" rel="stylesheet">
+	<link type="text/css" href="/footer/footer.css" rel="stylesheet">
+  </head>
+ <body>
+<div class="header">
+<?php include 'C:/OSPanel/domains/Bellyful/header/header.php'; ?>
+</div>
+
+<div class="Stock_img">
+	
+</div>
+
+
+<main role="main" class="main">
+  <section class="text-center" id="zagolovoc">
+    <div class="container">
+      <h5><b>Акции</b></h5>
+      <p>Подзаголовок<br>В несколько строк</p>
+    </div>
+  </section>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+              <div class="img_wrapper_1"><img src="/img/StockTea.png" id="img_style"></div>
+               <div class="Look_more">Подробнее</div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+          <div class="img_wrapper_1"><img src="/img/StockTea.png" id="img_style"></div>
+           <div class="Look_more">Подробнее</div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+          <div class="img_wrapper_1"><img src="/img/StockTea.png" id="img_style"></div>
+           <div class="Look_more">Подробнее</div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+          <div class="img_wrapper_1"><img src="/img/StockTea.png" id="img_style"></div>
+           <div class="Look_more">Подробнее</div>
+        </div>
+
+        
+
+    </div>
+</div>
+
+
+ <div class="NextPrev">
+  <button class="Prev"><a href="#"><img src="/img/prev.png">&emsp;<b>Назад</b></a></button>
+  <button class="Next"><a href="#"><b>Далее</b>&emsp;<img src="/img/next.png"></a></button>
+ </div>
+
+  <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+              <div class="img_wrapper_2"><img src="/img/StockOffers.png" id="img_style"></div>
+              <p>
+                <h4><b>Заголовок</b></h4>
+                Равным образом рамки и место<br>
+                обучения кадров способствует<br>
+                подготовки и реализаци<br>
+                дальнейших направлений развития.<br>
+                Равным образом постоянное<br>
+                информационное обеспечение<br>
+                нашей деятельности в значительной<br>
+                степени обусловливает создание<br>
+                модели развития.<br>
+             </p>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+          <div class="img_wrapper_2"><img src="/img/StockOffers.png" id="img_style"></div>
+             <p>
+                <h4><b>Заголовок</b></h4>
+                Равным образом рамки и место<br>
+                обучения кадров способствует<br>
+                подготовки и реализаци<br>
+                дальнейших направлений развития.<br>
+                Равным образом постоянное<br>
+                информационное обеспечение<br>
+                нашей деятельности в значительной<br>
+                степени обусловливает создание<br>
+                модели развития.<br>
+             </p>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+          <div class="img_wrapper_2"><img src="/img/StockOffers.png" id="img_style"></div>
+              <p>
+                <h4><b>Заголовок</b></h4>
+                Равным образом рамки и место<br>
+                обучения кадров способствует<br>
+                подготовки и реализаци<br>
+                дальнейших направлений развития.<br>
+                Равным образом постоянное<br>
+                информационное обеспечение<br>
+                нашей деятельности в значительной<br>
+                степени обусловливает создание<br>
+                модели развития.<br>
+             </p>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+              <div class="img_wrapper_2"><img src="/img/StockOffers.png" id="img_style"></div>
+              <p>
+                <h4><b>Заголовок</b></h4>
+                Равным образом рамки и место<br>
+                обучения кадров способствует<br>
+                подготовки и реализаци<br>
+                дальнейших направлений развития.<br>
+                Равным образом постоянное<br>
+                информационное обеспечение<br>
+                нашей деятельности в значительной<br>
+                степени обусловливает создание<br>
+                модели развития.<br>
+             </p>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+          <div class="img_wrapper_2"><img src="/img/StockOffers.png" id="img_style"></div>
+             <p>
+                <h4><b>Заголовок</b></h4>
+                Равным образом рамки и место<br>
+                обучения кадров способствует<br>
+                подготовки и реализаци<br>
+                дальнейших направлений развития.<br>
+                Равным образом постоянное<br>
+                информационное обеспечение<br>
+                нашей деятельности в значительной<br>
+                степени обусловливает создание<br>
+                модели развития.<br>
+             </p>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+          <div class="img_wrapper_2"><img src="/img/StockOffers.png" id="img_style"></div>
+              <p>
+                <h4><b>Заголовок</b></h4>
+                Равным образом рамки и место<br>
+                обучения кадров способствует<br>
+                подготовки и реализаци<br>
+                дальнейших направлений развития.<br>
+                Равным образом постоянное<br>
+                информационное обеспечение<br>
+                нашей деятельности в значительной<br>
+                степени обусловливает создание<br>
+                модели развития.<br>
+             </p>
+        </div>
+
+
+    </div>
+</div>
+
+</main>
+
+
+
+<div class="Footer">
+<?php include 'C:/OSPanel/domains/Bellyful/footer/footer.php'; ?>
+</div>
+
+ <!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="/js/popper.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/examples/vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="/js/main.js"></script>
+
+<script>
+	$(document).ready(function()
+	    {
+			$('#sidebarCollapse').on('click',function()
+			{
+				$('#sidebar').toggleClass('active');
+			});
+		});  
+
+</script>
+    
+<script>
+function openNav()
+{
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() 
+{
+    document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+    
+
+</body>
+</html>
